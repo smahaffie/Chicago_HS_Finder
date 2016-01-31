a08c6eeccfefa743df600ef4e5cda497f90324c4
Sherry Shenker <sshenker@cs.uchicago.edu>
2016-01-31 17:21:54 -0600

    working on csv

--- calculate_score.py
+++ calculate_score.py
@@ -1,52 +1,39 @@
 '''function that takes NWEA Reading Percentile, NWEA Math Percentile, 
 7th Grade Reading Grade, 7th Grade Math Grade,7th Grade Science Grade,
-7th Grade Social Studies Grade, and tier and returns score range 
+7th Grade Social Studies Grade, and tier and returns score range '''
 
-name = "readingTest"
-name = "mathTest"
-name = "readingGrade"
-name = "mathGrade"
-name = "scienceGrade"
-name = "socialGrade"
-name = "tier"
-
-import requests
-data = {'readingTest':80,'mathTest':90,'readingGrade':'A','mathGrade':'B',
-'scienceGrade':'A-','socialGrade':'B+','tier':1}
-
-url = "http://www.cpsoae.org/apps/pages/index.jsp?uREC_ID=72696&type=d&termREC_ID&pREC_ID=545282"
-
-r = requests.post(url, params = data )
-print(r.content)
-
-'''
-import urllib
-import requests
+import re
 import util
+import bs4
+import csv
 
-url = "http://www.cpsoae.org/apps/pages/index.jsp?uREC_ID=72696&type=d&termREC_ID&pREC_ID=545282"
+inputs = {"readingTest": 90, "mathTest": 90, "readingGrade": "A", "mathGrade": "B", "scienceGrade": "B+", "socialGrade": "A-", "tier": 1}
+grade_values = {"A": 75, "B":50, "C": 25, "D": 0, "F":0 }
+school_ranges = {"Brooks":{1:(602,739),2:(665,744),3:(690,746),4:655,}}
+csv = 'Data/Cutoff_Scores_2015-2016.csv'
 
-url2 = util.get_request(url)
+def create_min_max_dict(csv):
+	school_ranges = {}
+	with open(csv) as f:
+		f.readline()
+		for row in f:
+			fields = row.strip().split(",")
+			if fields[0] not in school_ranges:
+				school_ranges[fields[0]] = {"".format(fields[1],fields[2])}
 
 
-form_data = {'readingTest':80,'mathTest':90,'readingGrade':'A','mathGrade':'B',
-'scienceGrade':'A-','socialGrade':'B+','tier':1}
-
-params = urllib.parse.urlencode(form_data)
-req = urllib.request.request(url,params)
-response = urllib.request.urlopen(req)
-data = response.read()
 
 
+def calculate_scores(inputs,school_ranges, grade_values):
+	point_ranges = {}
+	#check test scores are integers between 0 and 100
+	assert inputs[readingTest] <= 100 and inputs[readingTest] >= 0, "Please enter valid number of points between 0 and 100"
+	assert inputs[mathTest] <= 100 and inputs[readingTest] >= 0, "Please enter valid number of points between 0 and 100"
+	test_scores = round(inputs[readingTest] * 1.515) + round(inputs[mathTest] * 1.515)
+	grade_pts = grade_values[readingGrade] + grade_values[mathGrade] + grade_values[scienceGrade] + grade_values[socialGrade]
+	total_pts = test_scores + grade_pts
+	for i in school_ranges:
+		max_ = schoolranges[i][inputs[tier]][1] - total_pts
+		min_ = schoolranges[i][inputs[tier]][0] - total_pts
+		point_ranges[i] = (min_,max_)
 
-'''
-
-br = mechanize.Browser()
-br.set_all_readonly(False)    # allow everything to be written to
-br.set_handle_robots(False)   # ignore robots
-br.set_handle_refresh(False)
-response = br.open(url)
-
-for form in br.forms():
-    print("Form name:", form.name)
-    print(form)'''
