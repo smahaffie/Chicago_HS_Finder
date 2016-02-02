d4b94446be0cdefba1d68af52e65468235a6e85d
Sherry Shenker <sshenker@cs.uchicago.edu>
2016-02-01 22:07:39 -0600

    i think it works

--- calculate_score.py
+++ calculate_score.py
@@ -7,33 +7,51 @@ import util
 import bs4
 import csv
 
-inputs = {"readingTest": 90, "mathTest": 90, "readingGrade": "A", "mathGrade": "B", "scienceGrade": "B+", "socialGrade": "A-", "tier": 1}
+#inputs just for testing
+inputs = {"readingTest": 90, "mathTest": 90, "readingGrade": "A", "mathGrade": "B", "scienceGrade": "B", "socialGrade": "C", "tier": 1}
 grade_values = {"A": 75, "B":50, "C": 25, "D": 0, "F":0 }
-school_ranges = {"Brooks":{1:(602,739),2:(665,744),3:(690,746),4:655,}}
 csv = 'Data/Cutoff_Scores_2015-2016.csv'
 
 def create_min_max_dict(csv):
+	'''
+	Creates a python dictionary from csv file that lists poitns needed for each school by tier
+	'''
 	school_ranges = {}
-	with open(csv) as f:
+	with open(csv,'r') as f:
 		f.readline()
 		for row in f:
 			fields = row.strip().split(",")
+			key = "{}{}".format(fields[1],fields[2])
 			if fields[0] not in school_ranges:
-				school_ranges[fields[0]] = {"".format(fields[1],fields[2])}
+				school_ranges[fields[0]] ={key: (fields[5],fields[6])}
+			else:
+				school_ranges[fields[0]][key] = (fields[5],fields[6])
+	return school_ranges
 
 
 
-
-def calculate_scores(inputs,school_ranges, grade_values):
+def calculate_scores(inputs,grade_values,csv):
+	'''
+	Computes range of points needed on entrance exam for each selective enrollment school using 2015 data
+	Inputs:
+		inputs: student variables
+		grade_values: point system CPS uses to weight grades 
+		csv: csv file with point ranges for each school by tier
+	Returns:
+		dictionary of ranges by school for student's tier and current points attained
+	'''
+	schoolranges = create_min_max_dict(csv)
 	point_ranges = {}
 	#check test scores are integers between 0 and 100
-	assert inputs[readingTest] <= 100 and inputs[readingTest] >= 0, "Please enter valid number of points between 0 and 100"
-	assert inputs[mathTest] <= 100 and inputs[readingTest] >= 0, "Please enter valid number of points between 0 and 100"
-	test_scores = round(inputs[readingTest] * 1.515) + round(inputs[mathTest] * 1.515)
-	grade_pts = grade_values[readingGrade] + grade_values[mathGrade] + grade_values[scienceGrade] + grade_values[socialGrade]
+	assert inputs["readingTest"] <= 100 and inputs["readingTest"] >= 0, "Please enter valid number of points between 0 and 100"
+	assert inputs["mathTest"] <= 100 and inputs["readingTest"] >= 0, "Please enter valid number of points between 0 and 100"
+	test_scores = round(inputs["readingTest"] * 1.515) + round(inputs["mathTest"] * 1.515)
+	grade_pts = grade_values[inputs["readingGrade"]] + grade_values[inputs["mathGrade"]] + grade_values[inputs["scienceGrade"]] + grade_values[inputs["socialGrade"]]
 	total_pts = test_scores + grade_pts
-	for i in school_ranges:
-		max_ = schoolranges[i][inputs[tier]][1] - total_pts
-		min_ = schoolranges[i][inputs[tier]][0] - total_pts
-		point_ranges[i] = (min_,max_)
+	for school in schoolranges:
+		t = "Tier" + str(inputs["tier"])
+		max_ = int(schoolranges[school][t][1]) - total_pts
+		min_ = int(schoolranges[school][t][0]) - total_pts
+		point_ranges[school] = (min_,max_)
+	return point_ranges
 
