9751d0038bfe243c9f809a0b9a76d0424f0392f3
Joseph Day <joeday@cs.uchicago.edu>
2016-02-08 14:53:34 -0600

    Pushing again

--- Clean Data/ACTtable.py
+++ Clean Data/ACTtable.py
@@ -1,24 +1,63 @@
-import sqlite3
+import sqlite3, csv
 
 
-connection = sqlite3.connect(WHAT HERE)
-c = connection.cursor()
+#connection = sqlite3.connect('CHSF.db')
+#c = connection.cursor()
 
-create = '''CREATE TABLE act
-(school_id varchar(8),
-category varchar(20),
-category_type varchar(20),
-year integer,
-composite_score_mean real,
-total_tested integer);'''
+def create(categoryofinterest):
+    connection = sqlite3.connect('CHSF.db')
+    c = connection.cursor()
 
-c.execute(create)
+    if categoryofinterest == 'ACT': 
+        create = '''CREATE TABLE act
+        (school_id varchar(8),
+        category varchar(20),
+        category_type varchar(20),
+        year integer,
+        composite_score_mean real,
+        total_tested integer);'''
+        #constraint pk_act school_id????
+    elif categoryofinterest == 'CEP':
+        create = '''CREATE TABLE cep
+        (school_id varchar(8),
+        graduates integer,
+        enrollment_pct real);'''
 
+    c.execute(create)
 
-separate = '''.separator ","'''
-imported = '''.import cleaned_ACT.csv act'''
+def import_data(categoryofinterest):
 
-average = '''SELECT sum(total_tested*
-    composite_score_mean)/sum(total_tested) AS average
-    FROM act  WHERE year = 2015 AND category = "Overall" 
-    AND category_type = "Overall";'''
+    connection = sqlite3.connect('CHSF.db')
+    c = connection.cursor()
+
+    if categoryofinterest == 'ACT':
+        with open('cleaned_ACT.csv', 'r') as fin:
+            dr = csv.DictReader(fin)        
+            to_db = [(i['School ID'], i[' Category'], i[' Category Type'], i[' Year'], i[' Composite Score Mean'], i[' Total Tested']) for i in dr]
+        c.executemany("INSERT INTO act (school_id, category, category_type, year, composite_score_mean, total_tested) VALUES (?, ?, ?, ?, ?, ?);", to_db)
+
+    elif categoryofinterest == 'CEP':
+        with open('cleaned_CEP.csv', 'r') as fin:
+            dr = csv.DictReader(fin)
+            to_db = [(i['School ID'], i['Graduates'], i['Enrollment Pct']) for i in dr]            
+        c.executemany("INSERT INTO cep (school_id, graduates, enrollment_pct) VALUES (?, ?, ?);", to_db)
+
+def compute_average(categoryofinterest):
+    connection = sqlite3.connect('CHSF.db')
+    c = connection.cursor()
+
+    if categoryofinterest == 'ACT':
+        average = '''SELECT sum(total_tested*
+            composite_score_mean)/sum(total_tested) AS average
+            FROM act  WHERE year = 2015 AND category = "Overall" 
+            AND category_type = "Overall";'''
+
+    elif categoryofinterest == 'CEP':
+        average = '''SELECT * FROM cep;'''
+        # '''SELECT sum(graduates*enrollment_pct)/sum(graduates) AS average
+         #   FROM cep;'''
+
+    r = c.execute(average)
+
+    print(r.fetchall())
+
