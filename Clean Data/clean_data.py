5832e53a70acbf483e99f404f8cb6cdcc4e5ca9f
Sherry Shenker <sshenker@cs.uchicago.edu>
2016-02-14 18:47:10 -0600

    added cleaning code for FOT and school type

--- Clean Data/clean_data.py
+++ Clean Data/clean_data.py
@@ -47,3 +47,38 @@ with open("ACT.csv", 'r') as f, open("cleaned_ACT.csv",'w') as f1:
     f1.write("School ID, Category, Category Type, Year, Composite Score Mean, Total Tested" + '\n')
     for line in f:
         f1.write(line)
+
+
+
+COMMAND = '''awk 'BEGIN {FS = ","};{print $1","$3", "$4}' <'FOT_SchoolLevel_20151023.csv' > 'FOT.csv' '''
+subprocess.call(COMMAND, shell=True)
+with open("FOT.csv", 'r') as f, open("cleaned_FOT.csv",'w') as f1:
+    next(f)
+    next(f)
+    f1.write("School ID, On Track Rate, Total Number of Freshman" + '\n')
+    for line in f:
+        f1.write(line)
+
+COMMAND = '''awk 'BEGIN {FS = ","};{print $1","$3", "$5}' <'Assessment_data_9_12_schools.csv' > 'Assessment912.csv' '''
+subprocess.call(COMMAND, shell=True)
+with open("Assessment912.csv", 'r') as f, open("cleaned_Assessment912.csv",'w') as f1:
+    next(f)
+    f1.write("School ID, Network, Rating" + '\n')
+    for line in f:
+        f1.write(line)
+
+COMMAND = '''awk 'BEGIN {FS = ","};{print $1","$3", "$9}' <'Assessment_data_comboschools.csv' > 'Assessmentcombo.csv' '''
+subprocess.call(COMMAND, shell=True)
+with open("Assessmentcombo.csv", 'r') as f, open("cleaned_Assessmentcombo.csv",'w') as f1:
+    next(f)
+    f1.write("School ID, Network, Rating" + '\n')
+    for line in f:
+        f1.write(line)
+
+COMMAND = '''awk 'BEGIN {FS = ","};{print $1","$3", "$5}' <'Assessment_data_optionschools.csv' > 'Assessmentoptions.csv' '''
+subprocess.call(COMMAND, shell=True)
+with open("Assessmentoptions.csv", 'r') as f, open("cleaned_Assessmentoptions.csv",'w') as f1:
+    next(f)
+    f1.write("School ID, Network, Rating" + '\n')
+    for line in f:
+        f1.write(line)
