83db8f01abfb4996653603f639db419a44627c92
Sherry Shenker <sshenker@cs.uchicago.edu>
2016-02-20 16:02:31 -0600

    added to form

--- schoolfinder/matcher/views.py
+++ schoolfinder/matcher/views.py
@@ -10,9 +10,10 @@ import sqlite3
 
 class FinderForm(forms.Form):
 	your_address = forms.CharField(label='Your address', max_length = 100)
-	distance = forms.CharField(label="Distance you're willing to travel", max_length = 10, required=False)
+	distance = forms.CharField(label="Time you're willing to travel", max_length = 10, required=False)
+	d_priority = forms.ChoiceField(label = "How important is the transit time?", choices = [(1,1),(2,2), (3,3), (4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10)])
 	schooltype = forms.MultipleChoiceField(label = "School type", required = False, widget=forms.CheckboxSelectMultiple(), choices = [(1,"Neighborhood"),(2,"Selective Enrollement"), (3,"Career Academy"), (4,"Magnet"),(5,"Contract"),(6,"Special Needs")])
-
+	a_priority = forms.ChoiceField(label = "How important are academics?", choices = [(1,1),(2,2), (3,3), (4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10)])
 #temporary function
 def get_travel_info_transit(address, address2):
 	return 2000
