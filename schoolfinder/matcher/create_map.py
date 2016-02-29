361cea00f0d07782f8140b3ddf523c4a478f0e6d
Sherry Shenker <sshenker@cs.uchicago.edu>
2016-02-28 18:11:08 -0600

    map?

---
+++ schoolfinder/matcher/create_map.py
@@ -0,0 +1,24 @@
+
+import requests
+
+url = "https://maps.googleapis.com/maps/api/geocode/json"
+function initMap() {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+	});
+
+	for school in schools:
+		var lat_long = get_lat_long(school["address"])
+		var marker = new google.maps.Marker({
+			position: lat_long
+			map: map
+			title: school["name"]
+		})
+
+	
+
+
+
+}
+
+?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY"
