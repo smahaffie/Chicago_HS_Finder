a72de7c60ed752f013b2e8763afbf4221d7730b6
Sherry Shenker <sshenker@cs.uchicago.edu>
2016-03-06 14:03:27 -0600

    cleaning code

---
+++ Functions/min_max_selective_enrollment.py
@@ -0,0 +1,38 @@
+import csv
+import json
+
+#inputs just for testing
+csv = '../Clean Data/Data_Files/Cutoff_Scores_2015_2016'
+
+
+name_id_dict = {'King':609751, 'Brooks':609726, 'Hancock':609694, 'Jones': 609678, 'Lane':609720, 'Lindblom': 610391,
+'Northside':609749, 'Payton':609680, 'South Shore':610547, 'Westinghouse':609693, 'Young':609755}
+
+
+def create_min_max_dict(csv):
+    '''
+    Creates a json dictionary from csv file that lists points needed for each selective enrollment school by tier
+    Inputs:
+        csv, string filename with path
+    Returns:
+        json dictionary
+
+    '''
+    school_ranges = {}
+    with open(csv,'r') as f:
+        f.readline()
+        for row in f:
+            print(row)
+            fields = row.strip().split(",")
+            key = "{}{}".format(fields[1],fields[2])
+            print(key)
+            if name_id_dict[fields[0]] not in school_ranges:
+                name = name_id_dict[fields[0]]
+                school_ranges[name] ={key: (fields[5],fields[6])}
+            else:
+                print("add")
+                school_ranges[name][key] = (fields[5],fields[6])
+
+    with open("../Clean Data/Data_Files/school_ranges.json", 'w') as f:
+        print(school_ranges)
+        json.dump(school_ranges,f)
