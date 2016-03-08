655f76bc0d96696a963bb1277f295d6354cb1683
Sherry Shenker <sshenker@cs.uchicago.edu>
2016-03-07 22:41:50 -0600

    fixed up table

--- Clean Data/clean_school_categories.py
+++ Clean Data/clean_school_categories.py
@@ -1,11 +1,21 @@
-'''Further clean categorization of schools data'''
+'''This file contains code to create the master school list that we use. 
+It renames the school categories to be correct because CPS does not 
+indicate which schools are magnet and selective enrollment and it merges 
+multiple files together to create the data for the "main" table we use
+in our SQL database. It also contains code to pad csvs that are missing schools 
+with rows that contain just school IDS to ease the SQL querying process'''
+
 import csv
 import pandas as pd
+
 csv.register_dialect('piper', delimiter='|', quoting=csv.QUOTE_NONE)
 
 files = ["Data_Files/cleaned_Assessmentoptions.csv", "Data_Files/cleaned_Assessmentcombo.csv", "Data_Files/cleaned_Assessment912.csv"]
+
 magnets = ["DISNEY II HS", "VON STEUBEN HS", "CHICAGO AGRICULTURE HS", "CRANE MEDICAL HS", "DEVRY HS", "CURIE HS", "CLARK HS"]
+
 se = ["BROOKS HS", "JONES HS", "KING HS", "LANE TECH HS", "LINDBLOM HS", "NORTHSIDE PREP HS", "PAYTON HS", "SOUTH SHORE INTL HS", "WESTINGHOUSE HS", "YOUNG HS" ]
+
 IB_schools = ["Amundsen High School","Back of the Yards High School",
 "Bogan High School", "Bronzeville Scholastic Academy High School", "Clemente High School",
 "Curie Metropolitan High School", "Farragut High School","Hubbard High School", 
@@ -16,8 +26,17 @@ IB_schools = ["Amundsen High School","Back of the Yards High School",
 "Taft High School","Washington High School"]
 
 def rename_categories(files,magnets,se):
+    '''
+    rename school categories to be accurate
+    Inputs:
+        files, list of filenames being used as source for "main" SQL table
+        magnets, list of strings of school names
+        se, list of string of school names of selective enrollment schools
+    Returns:
+        creates new files labeled "final"
+    '''
+
     for f in files:
-        print(f)
         new = f[:-4] + "_final.csv"
         with open(f) as fin, open(new, 'w') as f1:
             dr = csv.DictReader(fin,fieldnames=["School ID","School Name","Network","Rating"],dialect = "piper")
@@ -49,44 +68,47 @@ def rename_categories(files,magnets,se):
 to_merge = ["Data_Files/cleaned_Assessmentoptions.csv", "Data_Files/cleaned_Assessmentcombo.csv", "Data_Files/cleaned_Assessment912.csv"]
 merge = ["Data_Files/cleaned_Assessmentoptions_final.csv", "Data_Files/cleaned_Assessmentcombo_final.csv", "Data_Files/cleaned_Assessment912_final.csv"]
 def merge_category_csvs(files):
+
+    '''
+    combine multiple CSVS being used for "main" SQL table into a single csv
+    Inputs:
+        files, list of filenames
+    Returns:
+        single file named "merged.csv"
+    '''
+
     merged = open("merged.csv", 'w')
     merged.write("School ID|School Name|Network|Rating" + '\n')
     for f in files:
         with open(f,'r') as fil:
             for line in fil:
-                print(line)
                 merged.write(line)
 
 def every_school_in_every_file(merged_file, list_of_incomplete_files, list_of_destinations ):
     '''ensures that every file in list of file has an entry
     for each school ID in merged by inputting null entry if not
-    already present'''
+    already present
+    Inputs:
+        merged_file, complete file to check against
+        list_of_incomplete_files, list of filenames to pad
+        list_of_destinations, list of new filenames to save the padded files to
+    Returns:
+        new file for each file in list_of_incomplete_files
+    '''
 
-    merged_df = pd.read_csv(merged_file, sep = '|')
+    merged_df = pd.read_csv(merged_file, sep = ',')
     merged = [school for school in list(merged_df['School ID'])]
-    #print(len(merged))
+
     n=0
     for filename in list_of_incomplete_files:
         incomplete_df = pd.read_csv(filename, sep = '|')
         incomplete = [school for school in list(incomplete_df['School ID'])]
         length = len(incomplete)
-        #print(length)
         missing = [school for school in merged if not school in incomplete]
-        #print(len(missing))
-        #missing_schools = {}
+
         for school in missing:
             length += 1
-            #s2 = pd.Series([school, None, None, None], index = list(incomplete_df.columns))
-            incomplete_df.loc[length] = [school, None, None, None] 
-            
-        incomplete_df.to_csv(list_of_destinations[n], sep = '|')
+            school1 = school.round(decimals = 0)
+            incomplete_df.loc[length] = [school1, None, None] 
+        incomplete_df.to_csv(list_of_destinations[n], sep = '|',index = False)
         n+=1
-        #print(incomplete_df.shape)
-        
-#code to create SQL table
-
-create = '''CREATE TABLE main
-        (school_id varchar(8),
-        name varchar(30),
-        school_type varchar(30),
-        rating varchar(3));'''
--- schoolfinder/matcher/forms.py
+++ schoolfinder/matcher/forms.py
@@ -12,10 +12,10 @@ class FinderForm2(forms.Form):
 class FinderForm(forms.Form):
     your_address = forms.CharField(label='Your address', max_length = 100)
     distance = forms.IntegerField(label="How many minutes are you willing to travel?", max_value = 10000, required = False, min_value = 1)
-    d_priority = forms.ChoiceField(label = "How important is the transit time?",  choices = [(1,1),(2,2), (3,3), (4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10)])
+    d_priority = forms.ChoiceField(label = "How important is the transit time? (10 being most important)",  choices = [(1,1),(2,2), (3,3), (4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10)])
     schooltype = forms.MultipleChoiceField(label = mark_safe("What types of schools are you interested in? (<a href='http://cps.edu/Schools/High_schools/Pages/Highschooltypes.aspx' target='_blank'> Read about your options here</a>)"),
         required = False, widget=forms.CheckboxSelectMultiple(), choices = 
         [('Neighborhood',"Neighborhood"),('Selective Enrollment',"Selective Enrollement"), ('Military Academy',"Military Academy"), 
         ('Magnet',"Magnet"),('Contract',"Contract"),('Special Needs',"Special Needs"),('Charter', 'Charter'),("International Baccalaureate", "International Baccalaureate")])
     
-    a_priority = forms.ChoiceField(label = "How important are academics?",  choices = [(1,1),(2,2), (3,3), (4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10)])
+    a_priority = forms.ChoiceField(label = "How important are academics? (10 being most important)",  choices = [(1,1),(2,2), (3,3), (4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10)])
