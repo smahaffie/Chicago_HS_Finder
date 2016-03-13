fcee776ed5585ac17b5ff9d7cf0c58068abd4d63
Sherry Shenker <sshenker@cs.uchicago.edu>
2016-03-12 22:23:10 -0600

    added header

--- Clean Data/min_max_selective_enrollment.py
+++ Clean Data/min_max_selective_enrollment.py
@@ -1,3 +1,10 @@
+'''
+This file contains the function that generates a json file to store
+ the data on the minimum and maximum points needed for entry to each 
+ selective enrollment school by tier. This is data that only needs to 
+ be generated once per year (it is not student specific). 
+'''
+
 import csv
 import json
 
@@ -14,17 +21,15 @@ def create_min_max_dict(csv):
     with open(csv,'r') as f:
         f.readline()
         for row in f:
-            print(row)
             fields = row.strip().split(",")
-            key = "{}{}".format(fields[1],fields[2])
-            print(key)
+            key = "{}{}".format(fields[1],fields[2]) #keys are the tier 
+
             if name_id_dict[fields[0]] not in school_ranges:
                 name = name_id_dict[fields[0]]
                 school_ranges[name] ={key: (fields[5],fields[6])}
+                
             else:
-                print("add")
                 school_ranges[name][key] = (fields[5],fields[6])
 
     with open("Data_Files/school_ranges.json", 'w') as f:
-        print(school_ranges)
         json.dump(school_ranges,f)
