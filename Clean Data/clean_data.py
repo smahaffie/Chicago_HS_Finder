017e1d440724364824f7e33471a91db556c78196
Sherry Shenker <sshenker@cs.uchicago.edu>
2016-03-14 18:44:27 -0500

    fixed parametrization

--- Clean Data/clean_data.py
+++ Clean Data/clean_data.py
@@ -4,7 +4,7 @@ This file contains two functions that clean the CSV files that we downloaded
 from the CPS website and one we received from a CPS intern that contains information
 about high schools. The files are then imported to the SQL database
 
-This code is original with some documentation used for using the subprocess commands
+This code is original with some documentation referenced for the subprocess commands
 '''
 
 import csv
@@ -13,7 +13,7 @@ import json
 
 def clean_data(collenrollpersist_filename, act_filename, freshontrack_filename, mainhs_filename, combohs_filename, optionshs_filename):
     '''
-    Contains all the commands to clean all of the csvs required to build the data base
+    Contains all the commands to clean the main school data csvs required to build the data base
     
     Inputs:
         collenrollpersist_filename, string
@@ -31,7 +31,7 @@ def clean_data(collenrollpersist_filename, act_filename, freshontrack_filename, 
     Clean college enrollment and persistence data
     '''
 
-    COMMAND = '''awk 'BEGIN {FS = "|"};{print $1"|"$3"|"$5"|"$10}' < collenrollpersist_filename > 'Data_Files/CEP.csv' '''
+    COMMAND = '''awk 'BEGIN {FS = "|"};{print $1"|"$3"|"$5"|"$10}' < ''' + collenrollpersist_filename ''' > 'Data_Files/CEP.csv' '''
     subprocess.call(COMMAND, shell=True)
     with open("Data_Files/CEP.csv",'r') as f, open("Data_Files/cleaned_CEP.csv",'w') as f1:
         next(f) 
@@ -42,7 +42,7 @@ def clean_data(collenrollpersist_filename, act_filename, freshontrack_filename, 
     Clean ACT data
     '''
 
-    COMMAND = '''awk 'BEGIN {FS = "|"};{print $2"|"$4"|"$5"|"$7"|"$12"|"$17}' < act_filename > 'Data_Files/ACT.csv' '''
+    COMMAND = '''awk 'BEGIN {FS = "|"};{print $2"|"$4"|"$5"|"$7"|"$12"|"$17}' < ''' + act_filename + ''' > 'Data_Files/ACT.csv' '''
     subprocess.call(COMMAND, shell=True)
     with open("Data_Files/ACT.csv", 'r') as f, open("Data_Files/cleaned_ACT.csv",'w') as f1:
         next(f)
@@ -55,7 +55,7 @@ def clean_data(collenrollpersist_filename, act_filename, freshontrack_filename, 
     Clean freshman on track data
     '''
 
-    COMMAND = '''awk 'BEGIN {FS = "|"};{print $1"|"$3"|"$4}' < freshontrack_filename > 'Data_Files/FOT.csv' '''
+    COMMAND = '''awk 'BEGIN {FS = "|"};{print $1"|"$3"|"$4}' < ''' + freshontrack_filename + ''' > 'Data_Files/FOT.csv' '''
     subprocess.call(COMMAND, shell=True)
     with open("Data_Files/FOT.csv", 'r') as f, open("Data_Files/cleaned_FOT.csv",'w') as f1:
         next(f)
@@ -68,7 +68,7 @@ def clean_data(collenrollpersist_filename, act_filename, freshontrack_filename, 
     Clean category and rating data contained in 3 csvs
     '''
 
-    COMMAND = '''awk 'BEGIN {FS = "|"};{print $1"|"$2"|"$3"|"$5}' < mainhs_filename > 'Data_Files/Assessment912.csv' '''
+    COMMAND = '''awk 'BEGIN {FS = "|"};{print $1"|"$2"|"$3"|"$5}' < ''' + mainhs_filename + ''' > 'Data_Files/Assessment912.csv' '''
     subprocess.call(COMMAND, shell=True)
     with open("Data_Files/Assessment912.csv", 'r') as f, open("Data_Files/cleaned_Assessment912.csv",'w') as f1:
         next(f)
@@ -77,10 +77,9 @@ def clean_data(collenrollpersist_filename, act_filename, freshontrack_filename, 
         next(f)
         f1.write("School ID|School Name|Network|Rating" + '\n')
         for line in f:
-            print(line)
             f1.write(line)
 
-    COMMAND = '''awk 'BEGIN {FS = "|"};{print $1"|"$2"|"$3"|"$9}' < combohs_filename > 'Data_Files/Assessmentcombo.csv' '''
+    COMMAND = '''awk 'BEGIN {FS = "|"};{print $1"|"$2"|"$3"|"$9}' < ''' + combohs_filename + '''> 'Data_Files/Assessmentcombo.csv' '''
     subprocess.call(COMMAND, shell=True)
     with open("Data_Files/Assessmentcombo.csv", 'r') as f, open("Data_Files/cleaned_Assessmentcombo.csv",'w') as f1:
         next(f)
@@ -91,7 +90,7 @@ def clean_data(collenrollpersist_filename, act_filename, freshontrack_filename, 
         for line in f:
             f1.write(line)
 
-    COMMAND = '''awk 'BEGIN {FS = "|"};{print $1"|"$2"|"$3"|"$5}' < optionshs_filename > 'Data_Files/Assessmentoptions.csv' '''
+    COMMAND = '''awk 'BEGIN {FS = "|"};{print $1"|"$2"|"$3"|"$5}' <''' + optionshs_filename + '''> 'Data_Files/Assessmentoptions.csv' '''
     subprocess.call(COMMAND, shell=True)
     with open("Data_Files/Assessmentoptions.csv", 'r') as f, open("Data_Files/cleaned_Assessmentoptions.csv",'w') as f1:
         next(f)
@@ -100,7 +99,6 @@ def clean_data(collenrollpersist_filename, act_filename, freshontrack_filename, 
         next(f)
         f1.write("School ID|School Name|Network|Rating" + '\n')
         for line in f:
-            print(line)
             f1.write(line)
 
 def create_website_file(website_csv_file):
@@ -111,7 +109,7 @@ def create_website_file(website_csv_file):
     
     Inputs:
         website csv file, string
-    Returns:
+    Side effects:
         cleaned website csv
         
     '''
