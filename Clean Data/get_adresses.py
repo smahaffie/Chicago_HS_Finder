81f751e902bff07cd0c4d461e0abd22f6f50364c
Shelby Mahaffie <smahaffie@cs.uchicago.edu>
2016-03-13 19:06:07 -0500

    got rid of functions folder

--- Clean Data/get_adresses.py
+++ Clean Data/get_adresses.py
@@ -1,40 +1,42 @@
+# We ran the code in this file only in order to create 
+# a dictionary of all the schools in our database, 
+# which we used with two Google Maps APIS:
+# the directions API and the geolocation API.
+
 import requests
 import bs4
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 def get_all():
-    '''Aggregate function of get, get2, and get3, 
-    which allows us to get all addresses for schools on our list
-    of plausible schools'''
+    '''
+    Returns a dictionary mapping the name of every school
+    in the CPS system to its address
+    '''
+
     addresses = {}
 
     driver = webdriver.Firefox()
     driver.get("http://schoolinfo.cps.edu/schoolprofile/SearchResults.aspx")
 
-    get(driver.page_source, addresses)
+    std_notin = [0, 51, 52, 53, 54]
 
-    for link in ['2','3','4','5']:
+    get(driver.page_source, addresses, std_notin)
+
+    for link in ['2','3','4','5','...', '7','8', '9', '10']:
         elements = driver.find_elements_by_tag_name("a")
         for element in elements:
             if element.text == link:
                 element.click()
                 break
-
-        a = driver.page_source
-        get(driver.page_source, addresses)
-
-    for link in ['...', '7','8', '9', '10']:
-        elements = driver.find_elements_by_tag_name("a")
-        for element in elements:
-            if element.text == link:
-                element.click()
-                break
-
-        a = driver.page_source
         time.sleep(0.2)
-        get2(driver.page_source, addresses)
+
+        if link in ['2', '3', '4', '5']:
+            get(driver.page_source, addresses, std_notin)
+        if link in ['...', '7', '8', '9', '10']:
+            get(driver.page_source, addresses, [50, 51])
 
 
     elements = driver.find_elements_by_tag_name("a")
@@ -46,10 +48,8 @@ def get_all():
             else:
                 element.click()
                 break
-
-    a = driver.page_source
     time.sleep(0.2)
-    get(driver.page_source, addresses)
+    get(driver.page_source, addresses, std_notin)
 
     for link in ['12','13']:
         elements = driver.find_elements_by_tag_name("a")
@@ -57,32 +57,29 @@ def get_all():
             if element.text == link:
                 element.click()
                 break
-
-        a = driver.page_source
         time.sleep(0.2)
-        get(driver.page_source, addresses)
+        get(driver.page_source, addresses, std_notin)
 
     elements = driver.find_elements_by_tag_name("a")
     for element in elements:
             if element.text == '14':
                 element.click()
                 break
-
-    a = driver.page_source
     time.sleep(0.2)
-    get3(driver.page_source, addresses)
+    get(driver.page_source, addresses, [0, 33, 34, 35, 36, 37])
 
 
     driver.close()
 
     return addresses
 
-def get(html, addresses):
+def get(html, addresses, notin):
     '''
-
+    updates the addresses dictionary to include
+    all the schools on one page. notin is a list of 
+    all the tags that don't correspond to school
+    table rows. 
     '''
-
-    print("BEFORE ", len(addresses))
     soup = bs4.BeautifulSoup(html, 'html5lib')
 
     results = []
@@ -91,7 +88,7 @@ def get(html, addresses):
     for row in rows:
         tds = row.find_all('td')
         if len(tds) == 7:
-            if count not in [0, 51, 52, 53, 54]:
+            if count not in notin:
                 results.append(tds)
             else:
                 print(count)
@@ -99,80 +96,8 @@ def get(html, addresses):
 
     tmp = 0
     for result in results:
-        try:
-            school_name = result[1].find('a').text
-        except:
-            print("error=",result, len(results))
-
+        school_name = result[1].find('a').text
         len_sn = len(school_name.split())
         address = " ".join(result[1].text.split()[len_sn:])
         addresses[school_name] = address
-        print(tmp, school_name)
         tmp += 1
-
-    print("AFTER ", len(addresses))
-  
-
-
-
-def get2(html, addresses):
-    print("BEFORE", len(addresses))
-    soup = bs4.BeautifulSoup(html, 'html5lib')
-
-    results = []
-    count = 0
-    rows = soup.find_all('tr')
-    for row in rows:
-        tds = row.find_all('td')
-        if len(tds) == 7:
-            if count not in [50, 51]:
-                results.append(tds)
-            else:
-                print(count)
-            count += 1
-
-    tmp = 0
-    for result in results:
-        try:
-            school_name = result[1].find('a').text
-        except:
-            print("error=",result, len(results))
-
-        len_sn = len(school_name.split())
-        address = " ".join(result[1].text.split()[len_sn:])
-        addresses[school_name] = address
-        print(tmp, school_name)
-        tmp += 1
-
-    print("AFTER", len(addresses))
-
-def get3(html, addresses):
-    print("BEFORE ", len(addresses))
-    soup = bs4.BeautifulSoup(html, 'html5lib')
-
-    results = []
-    count = 0
-    rows = soup.find_all('tr')
-    for row in rows:
-        tds = row.find_all('td')
-        if len(tds) == 7:
-            if count not in [0, 33, 34, 35, 36, 37]:
-                results.append(tds)
-            else:
-                print(count)
-            count += 1
-
-    tmp = 0
-    for result in results:
-        try:
-            school_name = result[1].find('a').text
-        except:
-            print("error=",result, len(results))
-
-        len_sn = len(school_name.split())
-        address = " ".join(result[1].text.split()[len_sn:])
-        addresses[school_name] = address
-        print(tmp, school_name)
-        tmp += 1
-
-    print("AFTER ", len(addresses))
