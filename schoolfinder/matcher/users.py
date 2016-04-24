fec20503f190cea1e910e3fab7b5ae59dd2d617c
josephday <joeday@uchicago.edu>
2016-04-24 01:28:43 -0500

    Update users.py
    added fns

--- schoolfinder/matcher/users.py
+++ schoolfinder/matcher/users.py
@@ -1,10 +1,22 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.models import authenticate
+from django.contrib.auth.models import authenticate, login
+
+#authenticate takes username, pass
+#login takes user, request
 
 def new_user(firstname, lastname, email, password):
     user = User.objects.create_user(firstname, email, password)
     user.last_name = lastname
     user.save()
 
-def change_password(username, newpassword):
-    pass
+def change_password(username, oldpassword, newpassword):
+    user = authenticate(username, oldpassword)
+    if user is not None:
+        u = User.objects.get(username)
+        u.set_password(newpassword)
+        u.save()
+    else:
+        print("Incorrect info provided")
+        
+
+    
